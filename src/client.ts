473afe7bc1e21883c9655b4fe9b01521fc608fd0
iraizo <groebner.loron@gmail.com>
2020-08-01 02:31:26 +0200

    :fire: Removed promise antipattern and added getindex()

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,9 @@
+import {CRestManager} from "./restManager";
+
 import {IPlayer} from "./interfaces/players";
 import {IBans} from "./interfaces/bans";
-
-import {CRestManager} from "./restManager";
 import {IServer} from "./interfaces/servers";
+import {ICompanyIndex} from "./interfaces/company";
 
 const restManager = new CRestManager();
 
@@ -11,55 +12,63 @@ const restManager = new CRestManager();
 
 export class CClient {
 
-    public getPlayer(id: number): Promise<IPlayer> {
-        return new Promise(async (resolve, reject) => {
-            await restManager.request("https://api.truckersmp.com/v2/player/" + id, "GET").then(async response => {
-                if(response.status == 200) {
-                    resolve(await response.json());
-                } else reject();
-            }), e => reject(e);
-        })
+    public async getPlayer(id: number) {
+        const response = await restManager.request("https://api.truckersmp.com/v2/player/" + id, "GET")
+        if (response.status == 200) {
+            const player: IPlayer = await response.json();
+            return player;
+        }
     }
 
-    public getBans(id: number): Promise<IBans[]> {
-        return new Promise(async (resolve, reject) => {
-            let bans: IBans[] = [];
-            await restManager.request("https://api.truckersmp.com/v2/bans/" + id, "GET").then(async response => {
-                if(response.status == 200) {
-                    const json =  await response.json();
-                    for(let i = 0; i < json["response"].length; i++) {
-                        bans.push(json["response"][i]);
-                    }
-                    resolve(bans);
-                } else reject();
-            }), e => reject(e);
-        })
+    public async getBans(id: number) {
+        let bans: IBans[] = [];
+        const response = await restManager.request("https://api.truckersmp.com/v2/bans/" + id, "GET")
+        if (response.status == 200) {
+            const json = await response.json();
+            for (let i = 0; i < json["response"].length; i++) {
+                bans.push(json["response"][i]);
+            }
+            return bans;
+        }
     }
 
-    public getServers(): Promise<IServer[]> {
-        return new Promise(async (resolve, reject) => {
-            let servers: IServer[] = [];
-            await restManager.request("https://api.truckersmp.com/v2/servers", "GET").then(async response => {
-                if(response.status == 200) {
-                    const json =  await response.json();
-                    for(let i = 0; i < json["response"].length; i++) {
-                        servers.push(json["response"][i]);
-                    }
-                    resolve(servers);
-                } else reject();
-            }), e => reject(e);
-        })
+    public async getServers() {
+        let servers: IServer[] = [];
+        const response = await restManager.request("https://api.truckersmp.com/v2/servers", "GET");
+        if (response.status == 200) {
+            for (let i = 0; i < response["response"].length; i++) {
+                servers.push(response["response"][i]);
+            }
+            return servers;
+        }
     }
 
-    public getGameTime(): Promise<number> {
-        return new Promise(async (resolve, reject) => {
-            await restManager.request("https://api.truckersmp.com/v2/game_time", "GET").then(async response => {
-                if(response.status == 200) {
-                    const json = await response.json();
-                    resolve(json["game_time"]);
+    public async getGameTime() {
+        const response = await restManager.request("https://api.truckersmp.com/v2/game_time", "GET");
+        if (response.status == 200) {
+            const json = await response.json()
+            return json["game_time"];
+        }
+    }
+
+    static CCompany = class {
+        public async getIndex() {
+            let companyIndex: ICompanyIndex = {response: {recent: [], featured_cover: [], featured: []}};
+            const response = await restManager.request("https://api.truckersmp.com/v2/vtc", "GET");
+            if (response.status == 200) {
+                const json = await response.json();
+
+                for (let key in Object.keys(json.response.recent)) {
+                    companyIndex.response.recent.push(json.response.recent[key]);
                 }
-            }), e => reject(e);
-        })
+                for (let key in Object.keys(json.response.featured)) {
+                    companyIndex.response.featured.push(json["response"]["featured"][key]);
+                }
+                for (let key in Object.keys(json.response.featured_cover)) {
+                    companyIndex.response.featured_cover.push(json["response"]["featured_cover"][key]);
+                }
+            }
+            return companyIndex;
+        }
     }
-
 }
