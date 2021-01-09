51a72019015892a1d584ef9691e9a5d0f6bb2a2d
iraizo <groebner.loron@gmail.com>
2021-01-09 17:59:58 +0100

    🎨 Added trucky steam structure

--- src/interfaces/trucky/steam.ts
+++ src/interfaces/trucky/steam.ts
@@ -1,20 +1,42 @@
-export interface ITruckySteamPlayer {
-    steamID?: string,
-    playerInfo: {
-        steamid: string,
-        communityvisibilitystate: string,
-        profilestate: number,
-        personaname: string,
-        commentpermission: number,
-        profileurl: string,
-        avatar: string,
-        avatarmedium: string,
-        avatarfull: string,
-        avatarhash: string,
-        personastate: number,
-        primaryclanid: string,
-        timecreated: string,
-        personastateflags: number
+import {IPlayer} from "../players";
+
+export interface ISteamUser {
+    championship: {
+        applied: boolean,
+        applyDate: any
     },
-    found: boolean
+    friends: [],
+    friendsLoaded: false, 
+    total_points: number,
+    level: number,
+    _id: string,
+    avatar: string,
+    avatarfull: string,
+    avatarmedium: string,
+    communityvisibilitystate: null,
+    createdAt: string,
+    lastlogoff: any,
+    loccityid: any,
+    loccountrycode: any,
+    locstatecode: any,
+    personaname: string,
+    personastate: number,
+    personastateflags: number,
+    primaryclanid: string,
+    profilestate: number,
+    profileurl: string,
+    realname: any,
+    steamid: string,
+    timecreated: number,
+    updatedAt: string,
+    truckersMPUser: number,
+    created: string,
+}
+
+export interface ISteam {
+    steamUser: ISteamUser,
+    truckersMPUser: IPlayer,
+    onlineStatus: {
+        online: boolean
+    }
 }
